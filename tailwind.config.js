d0fde5ac9ea50696313e01984204387a7f566fd5
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2024-10-09 08:53:27 -0300

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "black": {
+          "text": "",
+          "background": "",
+          "border": "",
+          "hover": "",
+          "active": "",
+        },
+        "white": {
+          "text": "",
+          "background": "",
+          "border": "",
+          "hover": "",
+          "active": "",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
